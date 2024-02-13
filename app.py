1f0aad0c6ff38d3bb1119541452b0248b2a9c27d
Marcela <bacovska.marcela@gmail.com>
2024-02-13 11:55:25 +0100

    Send a confirmation email to the applicant

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 from datetime import datetime
-
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "myapplication123"
+app.config["SECRET_KEY"] = "secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "email@gmail.com"
+app.config["MAIL_PASSWORD"] = "password"
+
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 
 class Form(db.Model):
@@ -34,6 +42,18 @@ def index():
         db.session.add(form)
         db.session.commit()
 
+        message_body = f"Thank you for your submission, {first_name}, " \
+                       f"Here are your data:\n{first_name}\n{last_name}\n{date}\n" \
+                       f"Thank you!"
+        message = Message(subject="New form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+        mail.send(message)
+
+        flash(f"{first_name}, your form was submitted successfully!",
+              "success")
+
     return render_template("index.html")
 
 
@@ -41,4 +61,3 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         app.run(debug=True, port=5001)
-
